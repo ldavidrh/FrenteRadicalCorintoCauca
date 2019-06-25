3ebca7e87ee65f13db52bf3eac4704648f6de311
Luis <luis.david.restrepo@correounivalle.edu.co>
2019-06-25 11:16:05 -0500

    connection pool implemented for better database connection perfomance and usability

--- server/conn/connection.js
+++ server/conn/connection.js
@@ -1,12 +1,12 @@
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
 //Creating a new client and passing the URI
 
-const client = new Client({
+const pool = new Pool({
     connectionString : 'postgres://chifuavyjdteuw:da94215e492472b304f57fc7d63c2c06fe2d8fc3f2d4b53c048e745d3fe5c559@ec2-107-20-230-70.compute-1.amazonaws.com:5432/d5j8frme84avk6',
     ssl : true,
 });
 
 
 //Exporting the client as a module so it can be used in another file
-module.exports = client;
+module.exports = pool;
