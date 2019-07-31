053a0501ef94b07b23130edab127ab3dcd4dc0f3
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-07-31 08:52:55 -0500

    deleted cliente app

--- madlab/settings.py
+++ madlab/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.pages',
-    'apps.clientes',
     'apps.productos',
     'apps.facturas',
     'apps.proovedores',
