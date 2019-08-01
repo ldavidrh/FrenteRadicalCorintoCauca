adc8963a17bf319ddd795d608a8be329f583da57
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-07-31 20:13:32 -0500

    maded some changes

--- apps/almacenes/models.py
+++ apps/almacenes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from apps.Productos import Producto
 # Create your models here.
+class Almacen(models.Model):
+    codigo = models.CharField(max_length=10, primary_key=True)
+    
+    #Ciudades
+    CALI = 'Cali'
+    BOGOTA = 'Bogota'
+    MEDELLIN = 'Medellin'
+    BARRANQUILLA = 'Barranquilla'
+    CARTAGENA = 'Cartagena'
+    CUCUTA = 'Cucuta'
+    IBAGUE = 'Ibague'
+    PASTO = 'Pasto'
+
+
+    CIUDAD_CHOICES={
+        (CALI, 'cali'),
+        (BOGOTA, 'bogota'),
+        (MEDELLIN, 'medellin'),
+        (BARRANQUILLA, 'barranquilla'),
+        (CARTAGENA, 'cartagena'),
+        (CUCUTA, 'cucuta'),
+        (IBAGUE, 'ibague'),
+        (PASTO, 'pasto'),
+    }
+
+    ciudad = models.CharField(
+        choices = CIUDAD_CHOICES,
+        max_value= 20,
+        default = CALI 
+    )
+
+    direccion = models.CharField(max_length=30)
--- apps/detalles/models.py
+++ apps/detalles/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from productos import Producto
 # Create your models here.
 class Detalle(models.Model):
-    codigo = models.CharField(max_length=7)
+    codigo = models.CharField(max_length=7, primary_key=True)
     descripcion = models.CharField(max_length=100)
     producto = models.ForeignKey(Producto, on_delete=CASCADE)
