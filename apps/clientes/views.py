5fe8d548d9c55d84fdac6d50e469b9f2605f0332
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-08-05 00:20:34 -0500

    ahora agrega usuarios pero carece de un frontend agradable

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -6,16 +6,16 @@ from django.contrib import messages
 # Create your views here.
 def registro(request):
     if request.method == 'POST':
-        forms = FormularioRegistroCliente(request.POST)
+        form = FormularioRegistroCliente(request.POST)
         if form.is_valid():
             instance = form.save(commit = False)
             numero_documento = form.cleaned_data.get('numero_documento')
             instance.username = numero_documento
             instance.save()
-            messsages.success(request, 'Cliente registrado exitosamente')
+            messages.success(request, 'Cliente registrado exitosamente')
             return redirect('inicio')
         
     else:
-        forms = FormularioRegistroCliente()
+        form = FormularioRegistroCliente()
     
-    return render(request, 'clientes/registro.html', {'form':forms})
+    return render(request, 'clientes/registro.html', {'form':form})
