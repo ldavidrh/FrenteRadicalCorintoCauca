f6cc102230676f4932326087aa2fcd9bcf36b5d4
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-08-20 20:18:48 -0500

    crud descuentos finalizado

--- apps/descuentos/urls.py
+++ apps/descuentos/urls.py
@@ -5,4 +5,6 @@ app_name = 'descuentos'
 
 urlpatterns = [
     path('registrar/', registrar_view, name = 'registrar'),
+    path('consultar/', consultar_view, name = 'consultar'),
+    path('modificar/<int:id>', modificar_view, name = 'modificar'),
 ]
--- apps/descuentos/views.py
+++ apps/descuentos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import FormularioCreacionDescuento
+from .models import Descuento
+from apps.categorias.models import Categoria
 
 # Create your views here.
 def registrar_view(request):
@@ -14,3 +16,24 @@ def registrar_view(request):
         form = FormularioCreacionDescuento()
 
     return render(request, 'descuentos/registro.html', {'form': form})
+
+
+def consultar_view(request):
+    descuentos = Descuento.objects.all().values()
+    categorias = Categoria.objects.all().values()
+    return render(request, 'descuentos/consultar.html', {'descuentos': descuentos, 'categorias': categorias})
+
+
+def modificar_view(request, id):
+    categorias = Categoria.objects.all().values()
+    descuento = Descuento.objects.get(pk = id)
+    if request.method == 'GET':
+        form = FormularioCreacionDescuento(instance = descuento)
+    else:
+        form = FormularioCreacionDescuento(request.POST, instance = descuento)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Descuento modificado exitosamente')
+        return redirect('descuentos:consultar')
+    
+    return render(request, 'descuentos/modificar.html', {'form':form, 'categorias':categorias})
